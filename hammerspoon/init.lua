642e05b7157a51aed44d1145f0faf630016c70a6
Noah Chase <noah@rocketmoney.com>
2026-01-15 13:06:59 -0500

    add comments and a window-cntering function. also, I guess prettier re-formatted this

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,57 +1,77 @@
 hs.window.animationDuration = 0 -- disable animations
 
-hs.hotkey.bind({"ctrl", "cmd"},  "h", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- left:
+hs.hotkey.bind({ "ctrl", "cmd" }, "h", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
 
-  if win:isFullScreen() then
-    return
-  end
+	if win:isFullScreen() then
+		return
+	end
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+
+	win:setFrame(f)
 end)
 
-hs.hotkey.bind({"ctrl", "cmd"},  "l", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- right:
+hs.hotkey.bind({ "ctrl", "cmd" }, "l", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
 
-  if win:isFullScreen() then
-    return
-  end
+	if win:isFullScreen() then
+		return
+	end
 
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+	f.x = max.x + (max.w / 2)
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+	win:setFrame(f)
 end)
 
-hs.hotkey.bind({"ctrl", "alt", "cmd"},  "space", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- full-screen:
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "space", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+	if win:isFullScreen() then
+		return
+	end
 
-  if win:isFullScreen() then
-    return
-  end
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w
+	f.h = max.h
+	win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
-  hs.reload()
+-- center:
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "C", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	if win:isFullScreen() then
+		return
+	end
+
+	f.x = max.w / 2 - (max.w / 4)
+	f.y = max.h / 2 - (max.h / 4)
+	win:setFrame(f)
+end)
+
+-- reload:
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
+	hs.reload()
 end)
 hs.alert.show("Config loaded")
